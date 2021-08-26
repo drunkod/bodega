9f81cb397d82036cb823f679a8f45aa0d4650209
Alex from Lia <alexlia@example.com>
2021-08-26 23:12:37 +0500

    add graphql hasura

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
+import{ createApp, h, provide } from 'vue'
+import { ApolloClient, ApolloLink, InMemoryCache, from, HttpLink } from '@apollo/client/core'
+import{ApolloClients } from '@vue/apollo-composable'
 import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import './index.css'
@@ -10,10 +12,40 @@ import messages from './i18n/ru'
   store.dispatch('fetchUser', user)
 }) */
 
+const token = <string>import.meta.env.VITE_HASURA_SECRET;
+const uri = <string>import.meta.env.VITE_HASURA_URL
+const additiveLink = from([
+  new ApolloLink((operation, forward) => {
+    operation.setContext(({ headers }: any) => ({
+      headers: {
+        ...headers,
+        'x-hasura-admin-secret': token
+      }
+    }));
+    return forward(operation);
+  }),
+  new HttpLink({ uri })
+]);
+
+const apolloClient = new ApolloClient({
+  link: additiveLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+});
+
 const i18n = createI18n({
   locale: 'ru',
   fallbackLocale: 'en',
   messages,
 })
 
-createApp(App).use(store).use(router).use(i18n).mount('#app')
+const app = createApp({
+  setup() {
+    provide(ApolloClients, {
+      default: apolloClient
+    });
+  },
+  render: () => h(App)
+});
+
+app.use(store).use(router).use(i18n).mount('#app')
